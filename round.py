d7b857edb9a0813a2ed31ad224131e0eeb1807b8
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-10-20 02:12:11 +0000

    updated round.py

--- round.py
+++ round.py
@@ -2,10 +2,11 @@
 
 # Created by: Daria Bernice Calitis
 # Created on: Oct 2021
-# This program formats your address to a mailing address
+# This program rounds numbers
 
 
 def round_number(entered_input, decimal_input):
+    # This functions rounds numbers with decimals
     rounded_num = entered_input[0] * 10.0 ** decimal_input
     rounded_num = int(rounded_num + 0.5)
     rounded_num = rounded_num / (10.0 ** decimal_input)
@@ -17,6 +18,7 @@ def main():
     # This function is the main function
     entered_input = []
 
+    # input
     try:
         number_input = input("Enter a number you want to round: ")
         number_input = float(number_input)
@@ -25,8 +27,10 @@ def main():
 
         entered_input.append(number_input)
 
+        # call function / process
         round_number(entered_input, decimal_input)
 
+        # output
         print("\nThe rounded number is {0}".format(entered_input[0]))
     except (Exception):
         print("\nInvalid Input.")
